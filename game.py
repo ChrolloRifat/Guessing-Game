87aa4ce6f9a65c71f18362365d76713fa2d93a2e
Chrollo <76785992+ChrolloRifat@users.noreply.github.com>
2021-12-11 16:57:55 +0600

    correct code game.py

--- game.py
+++ game.py
@@ -1,24 +1,28 @@
-store = 4 
-trial = 0 
-while trial < 3: 
-  entry = int(input("Guess the Number: ")) 
-if entry == store: 
-  print("Congratulations! You Won ") 
-  break 
-elif trial < 3: 
-  print("Try Again")
-    trial += 1
-else:
-    print("You failed !!!")
-print("You have reached Level 2!!") 
-store = store * 2 
-trial = 0 
-while trial < 3: 
-  entry = int(input("Guess the Number: "))
-if entry == store: print("Congratulations! You Won ") 
-  break
-elif trial < 3: 
-  print("Try Again") 
-  trial += 1 
-else: print("You failed !!!") 
+store = 4
+trial = 0
+while trial < 3:
+    entry = int(input("Guess the Number: "))
+    if entry == store:
+        print("Congratulations! You Won ")
+        break
+    elif trial < 3:
+        print("Try Again")
+
+        trial += 1
+    else:
+        print("You failed !!!")
+
+print("You have reached Level 2!!")
+store = store * 2
+trial = 0
+while trial < 3:
+    entry = int(input("Guess the Number: "))
+    if entry == store:
+        print("Congratulations! You Won ")
+        break
+    elif trial < 3:
+        print("Try Again")
+        trial += 1
+    else:
+        print("You failed !!!")
 print('You have completed the game!!')
